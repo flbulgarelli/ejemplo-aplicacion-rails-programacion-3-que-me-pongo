77d44e39a3e8d33fc179f1858dc6705a1461ad25
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-04-21 21:38:18 -0300

    Add prendas controller

---
+++ app/controllers/prendas_controller.rb
@@ -0,0 +1,10 @@
+class PrendasController < ApplicationController
+  # get /prendas/
+  def index
+  end
+
+  # get /prendas/:id
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  get '/prendas', to: 'prendas#index'
+  get '/prendas/:id', to: 'prendas#show'
 end
