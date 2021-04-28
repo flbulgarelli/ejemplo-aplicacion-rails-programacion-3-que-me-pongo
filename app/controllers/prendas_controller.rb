1d3706366332e2d985f023dc53825e23d96bbe42
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-04-28 19:50:23 -0300

    Listando prendas y mostrándolas desde la BBDD

--- app/controllers/prendas_controller.rb
+++ app/controllers/prendas_controller.rb
@@ -6,5 +6,22 @@ class PrendasController < ApplicationController
 
   # get /prendas/:id
   def show
+    @prenda = Prenda.find(params[:id])
   end
 end
+
+# 1. Cuando accedemos a /prendas/1, rails sabe
+# gracias a al routes.rb que hay que dirigir
+# a prendas#show, es decir, ejecutar el método
+# show de la clase (una instancia de la clase)
+# PrendasController
+#
+# 2. Luego el show en clase PrendasController accede a
+# los parámetros (mediante params) y busca en la
+# base de datos la prenda por id (find)
+#
+# 3. Implícitamente, al terminar de evaluar el método show, como no decimos nada en contrario,
+# se va a renderizar la vista views/prendas/show.hml.erb
+# 
+# 4. la vista correspondiente dibuja un html utilizando
+# la variable @prenda que fue seteada previamente en el controlador
